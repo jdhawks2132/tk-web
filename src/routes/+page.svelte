42e3a9b36db9e0adda0dd8217fdc366fc1ccca46
Joshua Hawks <jdhawks@gmail.com>
2023-04-01 12:55:41 -0500

    tailwind custom colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
 <h1 class="text-3xl font-bold my-11 text-center">⚡️ 😺 Under Construction 😺 ⚡️</h1>
+<!-- test tailwind custom colors  -->
+<!-- colors: {
+  'charcoal-gray': '#2D2D2D',
+  'medium-gray': '#9B9B9B',
+  'light-gray': '#F2F2F2',
+  'lightning-yellow': '#F2C94C'
+} -->
+
+<div class="bg-charcoal-gray">Some stuff</div>
+<div class="bg-medium-gray">Some stuff</div>
+<div class="bg-light-gray">Some stuff</div>
+<div class="bg-snapchat-yellow">Some stuff</div>
+<div class="bg-honey-yellow">Some stuff</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'charcoal-gray': '#2D2D2D',
+				'medium-gray': '#9B9B9B',
+				'light-gray': '#F2F2F2',
+				'snapchat-yellow': '#FFFC00',
+				'honey-yellow': '#FFB100'
+			}
+		}
+	},
+	plugins: []
+};
