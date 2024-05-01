47dbafb0a2056c5240ecb9190b2bdf76e8899f67
snl95 <snl95@txstate.edu>
2024-05-01 10:33:32 -0500

    schedule update 5/1 part 2

--- src/lib/stores/schedules.js
+++ src/lib/stores/schedules.js
@@ -1,90 +1,32 @@
 import { writable } from 'svelte/store';
 
 const schedules = [
-	// {
-	// 	id: 0,
-	// 	description: 'Practice',
-	// 	field: '1',
-	// 	location: 'Fair Oaks Park',
-	// 	jersey: 'N/A',
-	// 	googleMapsLink: 'https://maps.app.goo.gl/CZ59fg1mAq48eDaM6',
-	// 	imageUrl: '/images/fair_oaks.jpg',
-	// 	date: '4/13/24',
-	// 	time: '9:00 AM',
-	// 	isGame: false
-	// },
+
 	{
 		id: 1,
-		description: 'Pride',
-		field: '1',
-		location: 'McInnish Sporrts Complex',
-		jersey: 'Gray',
-		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
-		imageUrl: '/images/mckinnish.jpg',
+		description: 'Diesel',
+		field: 'TBD',
+		location: 'Spirit Park',
+		jersey: 'Black',
+		googleMapsLink: 'https://maps.app.goo.gl/GVmAxY3ir2Z6E9JF6',
+		imageUrl: '/images/spirit_park.jpg',
 		date: '5/4/24',
-		time: '8:00 AM',
+		time: '11:00 AM',
 		isGame: true
 	},
 	{
 		id: 2,
-		description: 'Homies',
-		field: '1',
-		location: 'McInnish Sporrts Complex',
-		jersey: 'Gray',
-		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
-		imageUrl: '/images/mckinnish.jpg',
+		description: 'Crew',
+		field: 'TBD',
+		location: 'Spirit Park',
+		jersey: 'Black',
+		googleMapsLink: 'https://maps.app.goo.gl/GVmAxY3ir2Z6E9JF6',
+		imageUrl: '/images/spirit_park.jpg',
 		date: '5/4/24',
-		time: '9:00 AM',
-		isGame: true
-	},
-	{
-		id: 3,
-		description: 'Base Invaders',
-		field: '2',
-		location: 'McInnish Sporrts Complex',
-		jersey: 'Gray',
-		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
-		imageUrl: '/images/mckinnish.jpg',
-		date: '5/4/24',
-		time: '12:00 PM',
-		isGame: true
-	},
-	{
-		id: 4,
-		description: 'Base Invaders',
-		field: '2',
-		location: 'McInnish Sporrts Complex',
-		jersey: 'Yellow',
-		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
-		imageUrl: '/images/mckinnish.jpg',
-		date: '5/5/24',
-		time: '1:00 PM',
-		isGame: true
-	},
-	{
-		id: 5,
-		description: 'Twisted Tea',
-		field: '1',
-		location: 'McInnish Sporrts Complex',
-		jersey: 'Yellow',
-		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
-		imageUrl: '/images/mckinnish.jpg',
-		date: '5/5/24',
 		time: '2:00 PM',
 		isGame: true
-	},
-	{
-		id: 6,
-		description: 'Twisted Tea',
-		field: '2',
-		location: 'McInnish Sporrts Complex',
-		jersey: 'Yellow',
-		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
-		imageUrl: '/images/mckinnish.jpg',
-		date: '5/5/24',
-		time: '3:00 PM',
-		isGame: true
 	}
+
 ];
 
 const schedulesStore = writable(schedules);
