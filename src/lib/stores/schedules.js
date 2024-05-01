a08561b375a80147c393764c6a6f813f0823caff
snl95 <snl95@txstate.edu>
2024-05-01 10:53:41 -0500

    add 5/5 games

--- src/lib/stores/schedules.js
+++ src/lib/stores/schedules.js
@@ -25,7 +25,31 @@ const schedules = [
 		date: '5/4/24',
 		time: '2:00 PM',
 		isGame: true
-	}
+	},
+	{
+		id: 3,
+		description: 'Osos',
+		field: '2',
+		location: 'McInnish Sporrts Complex',
+		jersey: 'Gray',
+		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
+		imageUrl: '/images/mckinnish.jpg',
+		date: '5/5/24',
+		time: '1:00 PM',
+		isGame: true
+	},
+	{
+		id: 4,
+		description: 'Kats',
+		field: '2',
+		location: 'McInnish Sporrts Complex',
+		jersey: 'Gray',
+		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
+		imageUrl: '/images/mckinnish.jpg',
+		date: '5/5/24',
+		time: '2:00 PM',
+		isGame: true
+	},
 
 ];
 
