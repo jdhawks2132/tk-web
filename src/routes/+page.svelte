cdeba9e7c0e48886e7d685a11a80e7f557fec8c4
snl95 <snl95@txstate.edu>
2025-02-11 08:40:33 -0600

    events update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -90,15 +90,15 @@
 <section id="events" class="py-12">
 	<h2 class="text-2xl font-bold mb-6">Our Events</h2>
 	<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-		<div
+		<!-- <div
 			class="w-11/12 hover:shadow-lg transition-shadow duration-200 hover:shadow-yellow-500 rounded-lg"
 		>
 			<a href="/events/2">
 				<img src="/images/uniform.jpg" alt="extra innings" class="event-image h-auto rounded-lg" />
 			</a>
-		</div>
+		</div> -->
 		<div
-			class="w-11/12 hover:shadow-lg transition-shadow duration-200 hover:shadow-yellow-500 rounded-lg"
+			class="w-11/12 hover:shadow-lg transition-shadow duration-200 hover:shadow-red-300 rounded-lg"
 		>
 			<a href="/events/3">
 				<img
