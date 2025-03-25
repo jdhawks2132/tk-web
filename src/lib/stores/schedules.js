b33a285f8b4fce2c81b87859593880e94a5fd18a
snl95 <snl95@txstate.edu>
2025-03-25 16:52:25 -0500

    sched 3.25

--- src/lib/stores/schedules.js
+++ src/lib/stores/schedules.js
@@ -2,39 +2,27 @@ import { writable } from 'svelte/store';
 
 const schedules = [
 	{
-		id: 13,
-		description: 'Practice',
-		field: '1',
-		location: 'Walnut Hill Park',
-		jersey: 'N/A',
-		googleMapsLink: 'https://maps.app.goo.gl/xVbKHZu3anJYkwUQ8',
-		imageUrl: '/images/walnut_hill.jpg',
-		date: '3/20/2025',
-		time: '7:00 PM',
-		isGame: false
-	},
-	{
 		id: 14,
-		description: 'Vengeance',
-		field: '1',
+		description: 'Big D',
+		field: '5',
 		location: 'McInnish Sports Complex',
-		jersey: 'White',
+		jersey: 'Black',
 		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
 		imageUrl: '/images/mckinnish.jpg',
-		date: '3/23/2025',
-		time: '11:00 AM',
+		date: '3/30/2025',
+		time: '12:00 PM',
 		isGame: true
 	},
 	{
 		id: 16,
-		description: 'Pride',
-		field: '1',
+		description: 'Twisted Tea',
+		field: '4',
 		location: 'McInnish Sports Complex',
-		jersey: 'White',
+		jersey: 'Black',
 		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
 		imageUrl: '/images/mckinnish.jpg',
-		date: '3/23/2025',
-		time: '12:00 PM',
+		date: '3/30/2025',
+		time: '1:00 PM',
 		isGame: true
 	}
 ];
