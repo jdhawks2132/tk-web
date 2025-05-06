eba255ac0d9d12fde94819fe4f65692edda866af
snl95 <snl95@txstate.edu>
2025-05-06 08:03:54 -0500

    schedule update rainout

--- src/lib/stores/schedules.js
+++ src/lib/stores/schedules.js
@@ -9,7 +9,7 @@ const schedules = [
 		jersey: 'White',
 		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
 		imageUrl: '/images/mckinnish.jpg',
-		date: '5/4/2025',
+		date: '5/11/2025',
 		time: '8:00 AM',
 		isGame: true
 	},
@@ -21,7 +21,7 @@ const schedules = [
 		jersey: 'White',
 		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
 		imageUrl: '/images/mckinnish.jpg',
-		date: '5/4/2025',
+		date: '5/11/2025',
 		time: '10:00 AM',
 		isGame: true
 	}
