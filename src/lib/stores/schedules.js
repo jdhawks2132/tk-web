968f5ea7d3b7552fb3a660264cf024913ab67894
snl95 <snl95@txstate.edu>
2025-06-24 11:22:12 -0500

    schedule update 6.24

--- src/lib/stores/schedules.js
+++ src/lib/stores/schedules.js
@@ -2,18 +2,6 @@ import { writable } from 'svelte/store';
 
 const schedules = [
 	{
-		id: 2,
-		description: 'Practice',
-		field: '5',
-		location: 'McInnish Sports Complex',
-		jersey: 'N/A',
-		googleMapsLink: 'https://goo.gl/maps/EeTL6Zy5ErWWM2nF9',
-		imageUrl: '/images/mckinnish.jpg',
-		date: '6/22/2025',
-		time: '8:00am - 10:00am',
-		isGame: false
-	},
-	{
 		id: 3,
 		description: 'Practice',
 		field: 'TBD',
